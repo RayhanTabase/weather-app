79325f9d32d19c1f1c1d06e5f643db15fad7d7c1
RayhanTabase <75745927+RayhanTabase@users.noreply.github.com>
2021-09-24 09:34:34 +0000

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,31 +1,3 @@
-function getAddress (latitude, longitude) {
-    return new Promise(function (resolve, reject) {
-        var request = new XMLHttpRequest();
-
-        var method = 'GET';
-        var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
-        var async = true;
-
-        request.open(method, url, async);
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    var data = JSON.parse(request.responseText);
-                    var address = data.results[0];
-                    resolve(address);
-                }
-                else {
-                    reject(request.status);
-                }
-            }
-        };
-        request.send();
-    });
-};
-
-
-
-
 
 function Weather() {
     const [position, setPosition] = React.useState([null,null])
